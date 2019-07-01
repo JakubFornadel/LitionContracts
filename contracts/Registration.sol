729f0ef18bacc8e660ed6a7e1fda4e3c02b6ff7e
ejossev <josef.sevcik@gmail.com>
2019-07-01 21:28:13 +0200

    external notary verifier

--- contracts/Registration.sol
+++ contracts/Registration.sol
@@ -1,16 +1,55 @@
-pragma solidity >=0.5.0;
-pragma experimental ABIEncoderV2;
+
+pragma solidity >=0.5.4;
+//pragma experimental ABIEncoderV2;
+import "contracts/ECVerify.sol";
 
 
 interface ChainValidator{
    function check_participant(uint vesting, address participant) external returns (bool);
+   function check_notary_data(bytes calldata data) external returns (address[] memory);  
 }
 
-contract DummyChainValidator{
-   function check_participant(uint vesting, address participant) pure public returns (bool){
+contract DummyChainValidator is ChainValidator, ECVerify{
+   function check_participant(uint vesting, address participant) public returns (bool){
       if(vesting > 100*10^18 )
          return true;
       return false;
+   }
+
+   function check_notary_data(bytes memory data) public returns (address[] memory sigs){
+      bytes32 hash;
+      int data_pointer = 32;//first 32 bytes is array length
+      require( data.length % 65 == 32 );
+      uint sig_count = (data.length - 32)/65;
+
+      sigs = new address[](sig_count);
+      assembly {
+         hash := mload(add(data, data_pointer))
+      }
+      data_pointer +=32;
+      
+      for(uint i =0; i<sig_count; i++) {
+         bytes32 r;
+         bytes32 s;
+         uint8 v;
+         assembly{
+            r := mload(add(data, data_pointer))
+            data_pointer := add(data_pointer,32)
+            s := mload(add(data, data_pointer))
+            data_pointer := add(data_pointer,32)
+            v := byte(0, mload(add(data, data_pointer)))
+            data_pointer := add(data_pointer, 1)
+         }
+         data_pointer += 65;
+         if (v < 27)
+         v += 27;
+         if (v != 27 && v != 28) {
+            sigs[i] = address(0);
+         } else {
+            bool recovered;
+            (recovered, sigs[i]) = safer_ecrecover(hash, v,r,s);
+         }
+      }
    }
 }
 
@@ -71,10 +110,13 @@ contract LitionRegistry{
    event NewChainEndpoint(uint id, string endpoint);
    event Deposit(uint indexed chain_id, uint deposit, address indexed depositer, uint256 datetime);
    event Vesting(uint indexed chain_id, uint deposit, address indexed depositer, uint256 datetime);
+   event StartMining(uint indexed chain_id, address miner);
+   event StopMining(uint indexed chain_id, address miner);
 
    ERC20 token;
    struct user_details{
       bool active;
+      bool mining;
       uint vesting;
       uint deposit;
       string endpoint;
@@ -117,7 +159,7 @@ contract LitionRegistry{
       _vest_in_chain( id, vesting, msg.sender );
    }
 
-  function has_vested( uint id, address user) view external returns (bool){
+   function has_vested( uint id, address user) view external returns (bool){
       return chains[id].users[user].vesting > 0; 
    }
 
@@ -125,21 +167,19 @@ contract LitionRegistry{
       return chains[id].users[user].deposit > 0; 
    }
 
-   function notary(uint id, uint notary_block, address[] memory miners, uint[] memory blocks_mined, address[] memory users, uint[] memory user_gas, uint largest_tx, signature[] memory signatures) public{
+   function notary(uint id, uint32 notary_block, address[] memory miners, uint32[] memory blocks_mined, address[] memory users, uint32[] memory user_gas, uint32 largest_tx, bytes memory notary_data) public{
       //first, calculate hash from miners, block_mined, users and user_gas 
       //then, do ec_recover of the signatures to determine signers
       //check if there is enough signers (total vesting of signers > 50% of all vestings)
       //then, calculate reward
-      
       chain_info storage chain = chains[id];
       require(chain.active, "Trying to report about non-existing chain");
-      chain.last_notary = notary_block;
 
-      uint total_mined = 0;
       uint involved_vesting = 0;
-      for(uint i = 0; i < miners.length; i++){
-         total_mined += blocks_mined[i];
-         involved_vesting += chain.users[miners[i]].vesting;
+
+      address[] memory miners_in_notary = chains[id].validator.check_notary_data(notary_data);
+      for (uint i=0; i<miners_in_notary.length; i++){
+         involved_vesting += chain.users[miners_in_notary[i]].vesting;
       }
 
       require(involved_vesting * 3/2 > chain.total_vesting);
@@ -159,16 +199,17 @@ contract LitionRegistry{
       }
       
       for( uint i = 0; i < miners.length - 1; i++ ){
-         uint miner_reward = blocks_mined[i] * total_cost / total_mined;
+         uint miner_reward = blocks_mined[i] * total_cost / (notary_block - chain.last_notary);
          token.transfer( miners[i], miner_reward );
          total_cost -= miner_reward;
-         total_mined -= blocks_mined[i];
       }
+      
+      chain.last_notary = notary_block;
 
       token.transfer( miners[miners.length - 1], total_cost );
 
    }
-   
+
    //TODO - rework so withdrawals are not processed immediatelly but after notary window
    function _vest_in_chain( uint id, uint vesting, address user ) private {
       if(vesting > 0 ){
@@ -193,7 +234,7 @@ contract LitionRegistry{
    }
    
    //TODO - rework so withdrawals are not processed immediatelly but after notary window
-   function _deposit_in_chain( uint id, uint deposit, address user ) public {
+   function _deposit_in_chain( uint id, uint deposit, address user ) private {
       if(deposit > 0){
          require( chains[id].active, "can't deposit into non-existing chain" );
       }
@@ -207,7 +248,20 @@ contract LitionRegistry{
       chains[id].users[user].deposit = deposit;
       emit Deposit(id, deposit, user, now);
    }
-
-
+   
+   function start_mining(uint id) public {
+      require(chains[id].active);
+      require(chains[id].users[msg.sender].vesting > 0);
+      chains[id].users[msg.sender].mining = true;
+      emit StartMining(id, msg.sender);
+   }
+ 
+   function stop_mining(uint id) public {
+      require(chains[id].active);
+      require(chains[id].users[msg.sender].vesting > 0);
+      chains[id].users[msg.sender].mining = false;
+      emit StopMining(id, msg.sender);
+   }
+   
 
 }
