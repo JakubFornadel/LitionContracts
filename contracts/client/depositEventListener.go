82482810cf9d71c64a5d1c73f75fe76ccaa01a63
Jakub Fonadel <jakub.fornadel@gmail.com>
2019-08-02 12:48:04 +0200

    deposit listener added

---
+++ contracts/client/depositEventListener.go
@@ -0,0 +1,139 @@
+package litionContractClient
+
+import (
+	"context"
+	"errors"
+	"math/big"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/event"
+	log "github.com/sirupsen/logrus"
+)
+
+type DepositEventListener struct {
+	initialized    bool
+	listening      bool
+	scClient       *Lition
+	eventChannel   chan *LitionDeposit
+	eventSubs      event.Subscription
+	filterChainId  []*big.Int
+	stopChannel    chan struct{}
+	stoppedChannel chan struct{}
+	mutex          sync.Mutex
+}
+
+func NewDepositEventListener(scClient *Lition, chainId *big.Int) (*DepositEventListener, error) {
+	p := new(DepositEventListener)
+
+	p.initialized = false
+	p.listening = false
+	p.mutex = sync.Mutex{}
+	p.scClient = scClient
+	p.filterChainId = []*big.Int{chainId}
+	err := p.Init()
+	if err == nil {
+		return p, nil
+	}
+
+	return nil, err
+}
+
+func (listener *DepositEventListener) Init() error {
+	listener.mutex.Lock()
+	defer listener.mutex.Unlock()
+
+	if listener.initialized == true {
+		return nil
+	}
+
+	var err error
+	listener.eventChannel = make(chan *LitionDeposit)
+	listener.eventSubs, err = listener.scClient.WatchDeposit(
+		&bind.WatchOpts{Context: context.Background(), Start: nil},
+		listener.eventChannel,
+		listener.filterChainId,
+		nil)
+
+	if err != nil {
+		log.Error(err)
+		close(listener.eventChannel)
+		return err
+	}
+
+	listener.stopChannel = make(chan struct{})
+	listener.stoppedChannel = nil // Stopped channel is created and deleted only in start function
+	listener.initialized = true
+
+	return nil
+}
+
+func (listener *DepositEventListener) DeInit() {
+	listener.Stop()
+
+	listener.mutex.Lock()
+	defer listener.mutex.Unlock()
+
+	if listener.initialized == false {
+		return
+	}
+
+	listener.eventSubs.Unsubscribe()
+	close(listener.eventChannel)
+	close(listener.stopChannel)
+	// close(listener.stoppedChannel) // stoppned channel is already closed when stopped listening
+	listener.initialized = false
+}
+
+func (listener *DepositEventListener) ReInit() error {
+	listener.DeInit()
+	return listener.Init()
+}
+
+func (listener *DepositEventListener) Start(f func(*LitionDeposit)) error {
+	if listener.initialized == false {
+		return errors.New("Trying to Start 'DepositEventListener' without previous initialization")
+	}
+	if listener.listening == true {
+		log.Warning("Trying to Start 'DepositEventListener', which is already listening.")
+		return nil
+	}
+
+	listener.stoppedChannel = make(chan struct{})
+	listener.listening = true
+	log.Info("DepositEventListener start listening")
+
+	// close the stoppedchan when this func exits
+	defer func() {
+		close(listener.stoppedChannel)
+		listener.listening = false
+	}()
+
+	for {
+		select {
+		case event := <-listener.eventChannel:
+			log.Info("New 'StartMining' event received.")
+			f(event)
+		case err := <-listener.eventSubs.Err():
+			return err
+		case <-listener.stopChannel:
+			log.Info("Signal to stop DepositEventListener received.")
+			return nil
+		}
+	}
+}
+
+func (listener *DepositEventListener) Stop() {
+	listener.mutex.Lock()
+	defer listener.mutex.Unlock()
+
+	if listener.listening == false {
+		return
+	}
+
+	close(listener.stopChannel)
+	// wait for it to have stopped
+	<-listener.stoppedChannel
+	listener.stopChannel = make(chan struct{})
+	log.Info("DepositEventListener successfully stopped")
+}
